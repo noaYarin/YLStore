dde690c1c8276d833f2e6bc823a8c09d018f8d50
yarin <yarin066@gmail.com>
2022-12-23 15:47:12 +0200

    Changed search pipe for more categories

--- frontend/src/app/pipes/search.pipe.ts
+++ frontend/src/app/pipes/search.pipe.ts
@@ -6,17 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(items: any[], keyword: string): any {
-    if (Array.isArray(items) && typeof keyword == 'string') {
-      return items.filter(item => {
-        return Object.keys(item).some(key => {
-          if (typeof item[key] == 'string') {
-            return item[key].toLowerCase().includes(keyword.toLowerCase());
-          }
-          return false;
-        })
-      })
-    }
-    return items;
+    return keyword ?
+      items.filter((item) =>
+        JSON.stringify(item).toLowerCase().includes(keyword.toLowerCase())
+      ) :
+      items;
   }
 
 }
