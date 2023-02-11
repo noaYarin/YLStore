beb612e7b70cbb3124d3ce7fef74a1c33192a4e1
yarin <yarin066@gmail.com>
2023-02-11 23:59:11 +0200

    Added more logic for auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { catchError, Observable } from 'rxjs';
+import { catchError, map, Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,39 @@ import { User } from '../interfaces/user';
 export class AuthService {
   private configUrl = "http://localhost:3000" ?? ''
   user?: User
-  constructor(private http: HttpClient) { }
+  private token: string = ''
+  private isAuthenticated: boolean = false
+  private authStatusListener = new Subject<boolean>()
+  constructor(private http: HttpClient, private router: Router) { }
 
-  signIn(user: User): Observable<User> {
-    return this.http.post<User>(this.configUrl + '/user/signIn', user).pipe(
+  getToken() {
+    return this.token
+  }
+
+  getIsAuth() {
+    return this.isAuthenticated
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable()
+  }
+
+  signIn(user: User) {
+    return this.http.post<any>(this.configUrl + '/user/signIn', user).pipe(
+      map((res) => {
+        this.user = res.user
+        this.token = res.token
+        this.isAuthenticated = true
+        this.saveAuthData(this.token)
+        this.authStatusListener.next(true)
+        this.user = this.getUserData()
+      }),
       catchError(err => {
         throw new Error(err.error)
       })
     );
   }
+
   signUp(user: User): Observable<User> {
     return this.http.post<User>(this.configUrl + '/user/signUp', user).pipe(
       catchError(err => {
@@ -25,4 +50,46 @@ export class AuthService {
       })
     );
   }
+
+  logOut() {
+    this.token = '';
+    this.user = {}
+    this.isAuthenticated = false
+    this.authStatusListener.next(false)
+    this.clearAuthData()
+    this.router.navigate(['/']);
+  }
+
+  autoAuthUser() {
+    let authData = this.getAuthData()
+    if (!authData) return
+    this.token = authData.token
+    this.isAuthenticated = true
+    this.authStatusListener.next(true)
+  }
+
+  getLoggedUser() {
+    return this.getAuthData()
+  }
+
+  getUserData() {
+    let authData = this.getAuthData()
+    if (!authData) return
+    return JSON.parse(atob(authData.token.split('.')[1]))
+  }
+
+  private saveAuthData(token: string) {
+    localStorage.setItem('token', token)
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token')
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token')
+    if (!token) return
+    return { token }
+  }
+
 }
