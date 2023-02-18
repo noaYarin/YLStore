26c54e2e4100a5c6f1e26825e13fd894b330f692
yarin <yarin066@gmail.com>
2023-02-19 00:06:51 +0200

    Added cors package

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 app = express();
+
 (mongoose = require("mongoose")),
   (host = process.env.HOST || "localhost"),
   (port = 3000 || process.env.PORT),
   (dbhost = process.env.DBHOST || "mongodb://0.0.0.0:27017"),
   (logger = require("./services/logger")),
   ((itemRouter = require("./routes/itemRouter")),
+  (cors = require("cors")),
   (userRouter = require("./routes/userRouter")));
 
 //Cors
+app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -21,7 +24,7 @@ app.use(function (req, res, next) {
 });
 
 //Middlewars
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({
     extended: true,
